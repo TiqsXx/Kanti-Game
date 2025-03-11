a41a07f182d39b873eb7ab4379c11001d40cb9c9
LetsFireDH <thi22p@hotmail.com>
2025-03-11 09:43:00 +0100

    FullScreen and Titletrack

--- Assets/scripts/SettingsMenu.cs
+++ Assets/scripts/SettingsMenu.cs
@@ -8,15 +8,32 @@ public class SettingsMenu : MonoBehaviour
     public AudioMixer audioMixer; //Erstellt eine Referenz für den Audiomixer
     public Slider VolumeSlider;
     public Dropdown QualityDropdown;
+    public Toggle FullScreenToggle;
 
 
     void Update()
     {
         float savedVol = PlayerPrefs.GetFloat("VolumeSliderValue", 0.5f); //Hier wird der Speicher ausgelesen
         int savedQua = PlayerPrefs.GetInt("QualityIndex", 3);
+        int savedFull = PlayerPrefs.GetInt("FullScreen", 1);
         VolumeSlider.value = savedVol; //Hier wird der Wert der Variable ausgelesen und gesetzt
         if (audioMixer != null) { audioMixer.SetFloat("MasterVolume", savedVol); }
         if (QualityDropdown != null) { QualityDropdown.value = savedQua; }
+        switch (savedFull)
+        {
+            case 0:
+                Screen.fullScreen = false;
+                FullScreenToggle.isOn = false;
+                break;
+            case 1:
+                Screen.fullScreen = true;
+                FullScreenToggle.isOn = true;
+                break;
+            default:
+                Screen.fullScreen = true;
+                FullScreenToggle.isOn = true;
+                break;
+        }
     }
 
     public void SetVolume(float volume)
@@ -35,7 +52,14 @@ public class SettingsMenu : MonoBehaviour
 
     public void SetFullscreen (bool isFullscreen)
     {
-        Screen.fullScreen = isFullscreen;
+        if (isFullscreen == true)
+        {
+            PlayerPrefs.SetInt("FullScreen", 1);
+        } else if (isFullscreen == false)
+        {
+            PlayerPrefs.SetInt("FullScreen", 0);
+        }
+            Screen.fullScreen = isFullscreen;
     }
 
     public void ShowStartScreen()
